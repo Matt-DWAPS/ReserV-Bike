c542af9c56688ef3e435181631ada05e2dec0148
Matt-DWJ <47449189+Matt-DWJ@users.noreply.github.com>
2020-09-13 11:29:07 +0200

    application JS :  - Mise en place du slider,  - Ajout des fonctionnalités de lecture automatique, next, previous, play, pause  - Mise en place du changement de slide avec boutons clavier

--- js/Slider.js
+++ js/Slider.js
@@ -6,6 +6,7 @@ class Slider {
         this.run = true;
         this.play();
 
+
         $('#next').click(() => {
             clearInterval(this.interval);
             this.next();
@@ -18,26 +19,37 @@ class Slider {
             this.play();
         });
 
-        $('#player').click(() => {
-            this.pause();
+        $('#pause').click(() => {
+            clearInterval(this.interval);
+            $('#pause').css('display', 'none');
+            $('#play').css('display', 'block');
         });
+
+        $('#play').click(() => {
+            this.play();
+            $('#pause').css('display', 'block');
+            $('#play').css('display', 'none');
+        });
+
+        document.addEventListener("keydown", this.keyboardAction.bind(this));
+
+    }
+
+    keyboardAction(event) {
+        if (event.key === "ArrowLeft") {
+            clearInterval(this.interval);
+            this.prev();
+            this.play();
+        }
+        if (event.key === "ArrowRight") {
+            clearInterval(this.interval);
+            this.next();
+            this.play();
+        }
     }
 
     play() {
         this.interval = setInterval(this.defiled.bind(this), 5000);
-    }
-// revoir la partie play pause
-    pause(){
-        if (this.play){
-            clearTimeout(this.interval);
-            $('.fa-play-circle').show();
-            $('.fa-pause').hide();
-        } else {
-            this.next();
-            this.play();
-            $('.fa-play-circle').hide();
-            $('.fa-pause').show();
-        }
     }
 
     next() {
