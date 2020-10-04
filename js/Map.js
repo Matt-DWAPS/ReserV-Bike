bc5b246532bca193196239377b48939de849fcca
Matt-DWJ <47449189+Matt-DWJ@users.noreply.github.com>
2020-10-04 12:59:34 +0200

    Map :  - Amélioration du code en Jquery

--- js/Map.js
+++ js/Map.js
@@ -64,14 +64,14 @@ class Map {
             // Transforme la réponse en un tableau
             let allStations = JSON.parse(reponse);
 
-            for (let station of allStations){
+            for (let station of allStations) {
                 let newStation = Object.create(mymap.stationObject);
 
                 newStation.init(station.name, station.address, station.position.lat, station.position.lng, station.status, station.bike_stands, station.available_bike_stands, station.available_bikes, station.last_update);
 
                 let iconBase = mymap.greenIcon;
 
-                if (newStation.status === "OPEN"){
+                if (newStation.status === "OPEN") {
                     newStation.status = " OUVERT";
                     if (newStation.available_bikes < 10) { // moins de 10 places restantes, il devient orange
                         iconBase = mymap.orangeIcon;
@@ -84,51 +84,49 @@ class Map {
                     newStation.available_bikes = 0;
                 }
                 let marker = L.marker([newStation.position.lat, newStation.position.lng], {icon: iconBase}).addTo(mymap.map)
-                marker.bindPopup("<p>" + station.name + "</p>"+"<p>" + station.address + "</p>");
+                marker.bindPopup("<p>" + station.name + "</p>" + "<p>" + station.address + "</p>");
 
 
                 //Au click hors station vide les champs
                 mymap.map.addEventListener("click", deselectStation)
 
-                function deselectStation(){
+                function deselectStation() {
                     $('#info_reserv h4').html('Veuillez selectionner votre station');
                     $('#info_reserv p').html('');
-                    document.getElementById("reservation_form").style.display= "none";
-                    document.getElementById("info_reserv").style.display= "block";
-                    document.getElementById("container_pre_reserv").style.display= "none";
+                    $('#reservation_form').css("display", "none");
+                    $('#info_reserv').css("display", "block");
+                    $('#container_pre_reserv').css("display", "none");
                 }
 
                 //Au click sur une station ajoute les infos la concernant
                 marker.addEventListener("click", selectStation);
 
-                function selectStation(){
-                    if (newStation.status === " FERMEE"){
-                        let button_reserv = document.getElementById("pre_reserv");
+                function selectStation() {
+                    if (newStation.status === " FERMEE") {
+                        let button_reserv = $('#pre_reserv');
                         button_reserv.disabled = true;
                         button_reserv.value = "Station fermée";
                     }
-                    document.getElementById("info_reserv").style.display= "block";
-                    document.getElementById("container_pre_reserv").style.display= "block";
-                    document.getElementById("reservation_form").style.display= "none";
-                    document.getElementById("title_station").innerHTML = "Station";
-                    document.getElementById("name_station").innerHTML = newStation.name;
-                    document.getElementById("address_station").innerHTML = newStation.address;
-                    console.log(newStation.available_bike_stands);
-                    document.getElementById("status_station").innerHTML = "Actuellement : " + newStation.status;
-                    document.getElementById("disponibility_bike_station").innerHTML = "Vélos disponible : " + newStation.available_bikes;
-                    document.getElementById("bike_stand_station").innerHTML = "Support vélos : " + newStation.bike_stands;
-                    document.getElementById("available_bike_stand_station").innerHTML = "Support vélos disponible : " + newStation.available_bike_stands;
+                    $('#info_reserv').css("display", "block");
+                    $('#container_pre_reserv').css("display", "block");
+                    $('#reservation_form').css("display", "none");
+                    $('#title_station').text("station");
+                    $('#name_station').text(newStation.name);
+                    $('#address_station').text(newStation.address);
+                    $('#status_station').text("Actuellement : " + newStation.status);
+                    $('#disponibility_bike_station').text("Vélos disponible : " + newStation.available_bikes);
+                    $('#bike_stand_station').text("Support vélos : " + newStation.bike_stands);
+                    $('#available_bike_stand_station').text("Support vélos disponible : " + newStation.available_bike_stands);
                 }
             }
 
 
-             document.getElementById("pre_reserv").addEventListener("click", displayReservation);
+            $('#pre_reserv').click(displayReservation);
 
             //Affiche/Cache les div d'infos et de reservations lors du click
-             function displayReservation(){
-                 let div = document.getElementById("reservation_form");
-                div.style.display = "block";
-                document.getElementById("info_reserv").style.display= "none";
+            function displayReservation() {
+                $('#reservation_form').css("display","block");
+                $('#info_reserv').css("display","none");
             }
 
         });
